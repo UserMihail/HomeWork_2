a212bf51545e081a16adf07de73bf769fbf09cc5
Mihail <horunzhy.m@yandex.ru>
2022-12-08 12:26:48 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+string Num(int num)
+{
+    if ((-1000 < num && num < -99) || (99 < num && num < 1000 ))
+        return $"{num} -> {num / 100 * 10 + num % 10}";
+    return "Номер не трехзначный!";
+}
+Console.WriteLine(Num(int.Parse(Console.ReadLine())));
